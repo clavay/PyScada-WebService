820425e250d278d272ef2c1bff6f8080a34d7e88
clavay <clavayssiere@univ-pau.fr>
2023-04-05 11:41:23 +0200

    add project informations
    use project informations
    update license and pyscada version

--- pyscada/webservice/__init__.py
+++ pyscada/webservice/__init__.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import pyscada
-
-__version__ = '0.7.1rc1'
-__author__ = 'Camille Lavayssière'
+__version__ = "0.8.0"
+__author__ = "Camille Lavayssiere"
+__email__ = "team@pyscada.org"
+__description__ = "WebService extension for PyScada a Python and Django based Open Source SCADA System"
+__app_name__ = "WebService"
 
 PROTOCOL_ID = 94
 
 parent_process_list = [{'pk': PROTOCOL_ID,
-                        'label': 'pyscada.webservice',
-                        'process_class': 'pyscada.webservice.worker.Process',
+                        'label': 'pyscada.' + __app_name__.lower(),
+                        'process_class': 'pyscada.' + __app_name__.lower() + '.worker.Process',
                         'process_class_kwargs': '{"dt_set":30}',
                         'enabled': True}]
